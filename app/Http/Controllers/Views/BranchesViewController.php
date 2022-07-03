987725c5e11ca83461575b3b405fa6b1d9b7d4d2
a7medKhalid <69413330+a7medKhalid@users.noreply.github.com>
2022-07-03 23:20:22 +0300

    update branch

--- app/Http/Controllers/Views/BranchesViewController.php
+++ app/Http/Controllers/Views/BranchesViewController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Views;
 use App\Actions\AllowedDashboardPages;
 use App\Http\Controllers\Controller;
 use App\Http\Controllers\ModelsCRUD\BranchController;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
@@ -15,7 +16,10 @@ class BranchesViewController extends Controller
 
         $user = $request->user();
         $allowedDashboardPages = $AllowedDashboardPagesService->execute($user);
-        return Inertia::render('Dashboard/Branches/index',['allowedDashboardPages' => $allowedDashboardPages]);
+
+        $employees = User::role('employee')->get();
+
+        return Inertia::render('Dashboard/Branches/index',['employees' => $employees, 'allowedDashboardPages' => $allowedDashboardPages]);
 
     }
 
@@ -23,7 +27,17 @@ class BranchesViewController extends Controller
         $branch_controller = new BranchController;
 
         $branchModel = $branch_controller->create($request);
+        return $branchModel;
 
+    }
+
+    public function updateBranch(Request $request){
+
+        $branch_controller = new BranchController;
+
+        $branchModel = $branch_controller->update($request);
+
+        return $branchModel;
     }
 
 
