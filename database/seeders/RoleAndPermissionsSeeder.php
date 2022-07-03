63ccc1f63db3d64a118fb1c0f39b88366a943bdc
a7medKhalid <69413330+a7medKhalid@users.noreply.github.com>
2022-07-03 16:22:16 +0300

    created roles seeder

--- database/seeders/RoleAndPermissionsSeeder.php
+++ database/seeders/RoleAndPermissionsSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
 use Spatie\Permission\Models\Role;
 
 class RoleAndPermissionsSeeder extends Seeder
@@ -15,15 +16,46 @@ class RoleAndPermissionsSeeder extends Seeder
      */
     public function run()
     {
-        // Roles
+        //Create Roles
         $adminRole = Role::create(['name' => 'admin']);
         $managerRole = Role::create(['name' => 'manager']);
         $employeeRole = Role::create(['name' => 'employee']);
+        $productManager = Role::create(['name' => 'productManager']);
 
 
 
-        //Permissions
-        //$adminRole->givePermissionTo(['assign roles' ,'Branches CRUD']);
+        //Create Permissions
+
+        //create view dashboard pages permissions
+
+        $adminDashboardPages = config('constants.adminDashboardPages');
+        $managerDashboardPages = config('constants.managerDashboardPages');
+        $employeeDashboardPages = config('constants.employeeDashboardPages');
+        $productManagerDashboardPages = config('constants.productManagerDashboardPages');
+
+        foreach ($adminDashboardPages as $page){
+            $permission = Permission::firstOrCreate(['name' => 'view ' . $page]);
+            $adminRole->givePermissionTo($permission);
+        }
+
+        foreach ($managerDashboardPages as $page){
+            $permission = Permission::firstOrCreate(['name' => 'view ' . $page]);
+            $managerRole->givePermissionTo($permission);
+        }
+
+        foreach ($employeeDashboardPages as $page){
+            $permission = Permission::firstOrCreate(['name' => 'view ' . $page]);
+            $employeeRole->givePermissionTo($permission);
+        }
+
+        foreach ($productManagerDashboardPages as $page){
+            $permission = Permission::firstOrCreate(['name' => 'view ' . $page]);
+            $productManager->givePermissionTo($permission);
+        }
+
+
+        //assign permissions to roles
+//        $adminRole->givePermissionTo(['assign roles' ,'Branches CRUD']);
 
     }
 }
