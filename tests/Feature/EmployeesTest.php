75f54d6459b1e201dbbf7f0a6a4156fc013a0543
a7medKhalid <69413330+a7medKhalid@users.noreply.github.com>
2022-07-03 22:01:10 +0300

    employee tests

--- tests/Feature/EmployeesTest.php
+++ tests/Feature/EmployeesTest.php
@@ -14,6 +14,33 @@ class EmployeesTest extends TestCase
      *
      * @return void
      */
+
+    public function test_admin_can_view_employees_page()
+    {
+
+        $user = User::whereName('admin')->first();
+
+        $this->actingAs($user);
+
+        $response = $this->get('dashboard/employees');
+
+
+        $response->assertStatus(200);
+    }
+
+    public function test_manager_can_view_employees_page()
+    {
+
+        $user = User::whereName('manager')->first();
+
+        $this->actingAs($user);
+
+        $response = $this->get('dashboard/employees');
+
+
+        $response->assertStatus(200);
+    }
+
     public function test_admin_can_create_employee_invite()
     {
 
