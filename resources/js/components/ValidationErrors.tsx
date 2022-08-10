98713ac27b31dcfb2f2f7c0e8498461d841e2241
a7medKhalid <69413330+a7medKhalid@users.noreply.github.com>
2022-08-10 15:32:34 +0300

    arabic validation

--- resources/js/components/ValidationErrors.tsx
+++ resources/js/components/ValidationErrors.tsx
@@ -6,7 +6,7 @@ export default function ValidationErrors({ errors }) {
             {Object.keys(errors).length > 0 && (
                 <div className="mb-4">
                     <div className="font-medium text-red-600">
-                        Whoops! Something went wrong.
+                        حصل خطأ ما!
                     </div>
 
                     <ul className="mt-3 list-disc list-inside text-sm text-red-600">
